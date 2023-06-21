3b6592c8d8cef384596fe96491f7f432588f36e8
Alexis Purslane <alexispurslane@pm.me>
2023-06-21 15:41:54 -0400

    Fix small bug

--- tads3.el
+++ tads3.el
@@ -1418,7 +1418,7 @@ match($0, /^([a-z]\\w+)\\(.*\\)/, a) { if (!seen[a[1]]++) print a[1], \"function
 match($0, /^[ \\t]+([a-z]\\w+)\\(.*\\)/, a) { if (!seen[a[1]]++) print a[1], \"method\", FILENAME, FNR }
 match($0, /^[A-Z]\\w+\\(([^)]+)\\)\\s*.*\\s*:\\s*.*$/, a) { if (!seen[a[1]]++) print a[1], \"verb\", FILENAME, FNR }
 match($0, /^Define[TAI]+Action\\(([^)]+)\\)$/, a) { if (!seen[a[1]]++) print a[1], \"action\", FILENAME, FNR }
-match($0, /^\\++\\s+(\\w+): .*$/, a) { if (!seen[a[1]]++) print a[1], \"sub-object\", FILENAME, FNR }
+match($0, /^\\++\\s*(\\w+):.*$/, a) { if (!seen[a[1]]++) print a[1], \"sub-object\", FILENAME, FNR }
 match($0, /^[ \\t]+(\\w+) = /, a) { if (!seen[a[1]]++) print a[1], \"property\", FILENAME, FNR }
 ")
 
